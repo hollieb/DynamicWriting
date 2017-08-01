2fad187899bfeda309fe82ca6813708bf2fd09cf
Hollie Bradley <hollie_b@me.com>
2017-08-01 16:37:10 -0600

    Update main.m

--- DynamicWriting/main.m
+++ DynamicWriting/main.m
@@ -1,9 +1,8 @@
 //
 //  main.m
-//  字符串转换为UIBezierPath实现动态写字效果
 //
-//  Created by long on 16/2/24.
-//  Copyright © 2016年 long. All rights reserved.
+//  Created by Hollie Bradley on 16/2/24.
+//  Copyright © 2016 Hollie Bradley. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
